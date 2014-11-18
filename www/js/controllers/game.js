6a5eefe61ea61ef78457df0fd548e0797d649cb6
caffgeek <gibble@gmail.com>
2014-11-18 10:25:56 -0600

    added statistics, improved game handling

---
+++ www/js/controllers/game.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    app.controller("GameController", ["$scope", function ($scope) {
+        $scope.game = { Frames: [] };
+
+        $scope.roll = function (roll) {
+            var frame = { Number: 1, Shots: [roll] };
+            $scope.game.Frames.push(frame);
+        };
+    }]);
+}());
