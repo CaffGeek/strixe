87e7fe7693fe16ef99c9635802ad7192641d3389
caffgeek <gibble@gmail.com>
2014-11-21 16:11:17 -0600

    packaged

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+C:\source\dungeon\strixe\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
+C:\source\dungeon\strixe\platforms\android\ant-build\CordovaApp.ap_ \
+C:\source\dungeon\strixe\platforms\android\ant-build\classes.dex \
+C:\source\dungeon\strixe\platforms\android\CordovaLib\ant-build\classes.jar \
